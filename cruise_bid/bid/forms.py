70d0994fbf5ea4fba8032feea05d58cf4660a001
Charlie Wade <charlie@silviaterra.com>
2016-04-06 07:25:24 -0400

    made revisions

--- cruise_bid/bid/forms.py
+++ cruise_bid/bid/forms.py
@@ -1,10 +1,12 @@
+from bid.models import Cruise
 from django import forms
+from django.forms import ModelForm
 
 class bidForm(forms.Form):
 	newBid = forms.DecimalField(max_digits=5, decimal_places=2)
 	
-class cruiseForm(forms.Form):
-	name = forms.CharField(max_length=200)
-	state = forms.CharField(max_length=200)
-	numPlots = forms.IntegerField()
 	
+class cruiseForm(ModelForm):
+    class Meta:
+        model = Cruise
+        fields = ['name', 'state', 'numPlots']
--- cruise_bid/bid/views.py
+++ cruise_bid/bid/views.py
@@ -12,37 +12,43 @@ def index(request):
 
 def placeBid(request, cruise_id):
     cruise = get_object_or_404(Cruise, pk=cruise_id)
-    print '1'
+    
     
     if request.method == 'POST':
-        print 'Post'
         form = bidForm(request.POST)
         if form.is_valid(): 
             postBid = form.cleaned_data['newBid']
             if postBid > cruise.bid:
                 cruise.bid = postBid
                 cruise.save()
+                #message = 'Success!'
+                allCruises = Cruise.objects.all()
+                context = {'allCruises': allCruises}
+                return render(request, 'bid/index.html', context)
+            else:
+                form.is_valid = False
+                message = 'Try again, your bid must exceed $%s/plot' % cruise.bid
+                return HttpResponse(message)
     else:
-        print 'get'
         form = bidForm()
+        
     	
-    print 'render'
     return render(request, 'bid/placeBid.html', {'cruise': cruise, 'form':form})
     
 def submitCruise(request):
     if request.method == 'POST':
-        print 'Post'
         newCruise = cruiseForm(request.POST)
         if newCruise.is_valid():
             Cruise.objects.create(name = newCruise.cleaned_data['name'],
                                 state = newCruise.cleaned_data['state'],
                                 numPlots = newCruise.cleaned_data['numPlots'],
                                 bid = 0)
+            allCruises = Cruise.objects.all()
+            context = {'allCruises': allCruises}
+            return render(request, 'bid/index.html', context)
     else:
         newCruise = cruiseForm()
     return render(request, 'bid/submitCruise.html', {'newCruise':newCruise})
     
     
-
-
 
